ef29bc6b5932fb33554bdf14cbedf273ead7cdc2
arturgso <arturgs.dev@gmail.com>
2026-02-04 14:11:07 -0300

    feat: Implement basic routing and setup Tailwind CSS
    - Add .tool-versions for Node.js and Bun.
    - Initialize bun.lock file with project dependencies.
    - Update package.json to include vue-router and Tailwind CSS related devDependencies.
    - Add postcss.config.js to configure Tailwind CSS and Autoprefixer.
    - Remove default App.vue content and replace with router-view.
    - Delete HelloWorld.vue component.
    - Configure main.ts to use Vue Router.
    - Create initial pages/Auth.vue and pages/Dashboard.vue components.
    - Create src/router/index.ts to define application routes.
    - Update src/style.css to use Tailwind CSS directives.
    - Add tailwind.config.js configuration file.

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <h1 class="text-4xl">Dashboard Page</h1>
+</template>
---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import Dashboard from "../pages/Dashboard.vue";
+import Auth from "../pages/Auth.vue";
+import {createRouter, createWebHistory} from "vue-router";
+
+const routes = [
+    {
+        path: '/',
+        name: 'dashboard',
+        component: Dashboard
+    },
+    {
+        path: '/auth',
+        name: 'auth',
+        component: Auth
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router;
