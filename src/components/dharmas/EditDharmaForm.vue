4e4e6e756ef685c9b268d3299cbdd86be8922744
arturgso <arturgs.dev@gmail.com>
2026-02-18 11:10:34 -0300

    feat: Update pre-commit and pre-push hooks for improved formatting and validation

--- src/components/dharmas/EditDharmaForm.vue
+++ src/components/dharmas/EditDharmaForm.vue
@@ -7,31 +7,30 @@ import ActionButton from '../ui/ActionButton.vue';
 import { Trash } from 'lucide-vue-next';
 
 const props = defineProps<{
-    dharma: Dharma
+    dharma: Dharma;
 }>();
 
 const form = ref<EditDharmaProps>({
     name: props.dharma.name,
     color: props.dharma.color,
-    hidden: props.dharma.hidden
-})
-
+    hidden: props.dharma.hidden,
+});
 </script>
 
 <template>
     <form class="mt-4" @submit.prevent="">
         <div :class="styles.input.inputDiv">
-            <label for="name">
-                Nome
-            </label>
-            <input id="name" :class="styles.input.defaultInput"  :placeholder="dharma.name"/>  
+            <label for="name"> Nome </label>
+            <input id="name" :class="styles.input.defaultInput" :placeholder="dharma.name" />
         </div>
-        <ColorPicker v-model:color="form.color" /> 
+        <ColorPicker v-model:color="form.color" />
         <div class="flex items-center gap-2">
-        <ActionButton type="submit" text="Salvar" />
-        <button class="p-2 bg-red-500 rounded-md font-bold text-white  mt-4 hover:bg-red-700 transition duration-150 hover:shadow-lg ">
-        <Trash :size="28"/>
-        </button>
+            <ActionButton type="submit" text="Salvar" />
+            <button
+                class="p-2 bg-red-500 rounded-md font-bold text-white mt-4 hover:bg-red-700 transition duration-150 hover:shadow-lg"
+            >
+                <Trash :size="28" />
+            </button>
         </div>
     </form>
 </template>
