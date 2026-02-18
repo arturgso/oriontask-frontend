313c4cd211abad9c6533bf35229bbae6327e05f0
arturgso <arturgs.dev@gmail.com>
2026-02-18 11:07:04 -0300

    feat: Add delete button to EditDharmaForm component

--- src/components/dharmas/EditDharmaForm.vue
+++ src/components/dharmas/EditDharmaForm.vue
@@ -4,6 +4,7 @@ import type { Dharma, EditDharmaProps } from '@/types/Dharma';
 import { ref } from 'vue';
 import ColorPicker from './ColorPicker.vue';
 import ActionButton from '../ui/ActionButton.vue';
+import { Trash } from 'lucide-vue-next';
 
 const props = defineProps<{
     dharma: Dharma
@@ -26,6 +27,11 @@ const form = ref<EditDharmaProps>({
             <input id="name" :class="styles.input.defaultInput"  :placeholder="dharma.name"/>  
         </div>
         <ColorPicker v-model:color="form.color" /> 
+        <div class="flex items-center gap-2">
         <ActionButton type="submit" text="Salvar" />
+        <button class="p-2 bg-red-500 rounded-md font-bold text-white  mt-4 hover:bg-red-700 transition duration-150 hover:shadow-lg ">
+            <Trash :size="28"/>
+        </button>
+        </div>
     </form>
 </template>
